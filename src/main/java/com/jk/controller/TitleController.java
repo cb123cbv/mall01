cd2a1a2738782e73a00e03ca724cdda1ceb92988
chen <2439146769@qq.com>
2019-01-26 19:03:02 +0800

    陈

--- src/main/java/com/jk/controller/TitleController.java
+++ src/main/java/com/jk/controller/TitleController.java
@@ -10,6 +10,7 @@ import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.multipart.MultipartFile;
 
 import javax.annotation.Resource;
 import javax.swing.filechooser.FileSystemView;
@@ -29,7 +30,7 @@ public class TitleController {
     @RequestMapping("queryTitleInfo")
     @ResponseBody
     public SendPage queryTitleInfo(ReceivePage receivePage, TitleInfo titleInfo) {
-        SendPage titleList = titleService.queryTitleInfo(receivePage,titleInfo);
+        SendPage titleList = titleService.queryTitleInfo(receivePage, titleInfo);
         return titleList;
     }
 
@@ -57,42 +58,55 @@ public class TitleController {
         titleService.deletes(ids);
         return "1";
     }
-   String format="";
+
+
     //poi导出
+    String format = "";
+
     @ResponseBody
     @RequestMapping("exportExcel")
-    public String exportExcel(@RequestParam("id[]") String[] id) throws Exception{
-        String sheetName="标题列表";
-        String titleName="mall项目中的标题列表";
-        String[] headers = { "标题ID", "商品名称", "标题跳转路径"};
+    public String exportExcel(@RequestParam("id[]") String[] id) throws Exception {
+        String sheetName = "标题列表";
+        String titleName = "mall项目中的标题列表";
+        String[] headers = {"标题ID", "商品名称", "标题跳转路径"};
         List<TitleInfo> dataSet = titleService.getTitleList(id);
         /*生成桌面路径*/
         FileSystemView fsv = FileSystemView.getFileSystemView();
-        File com=fsv.getHomeDirectory();
-        SimpleDateFormat sb=new SimpleDateFormat("yyyy-MM-ddHHmmss");
+        File com = fsv.getHomeDirectory();
+        /*时间戳*/
+        SimpleDateFormat sb = new SimpleDateFormat("yyyy-MM-ddHHmmss");
         format = sb.format(new Date());
-        String resultUrl=com+"\\"+format+".xls";
-        String pattern="yyyy-MM-dd";
+        String resultUrl = com + "\\" + format + ".xls";
+        String pattern = "yyyy-MM-dd";
+
         ExportExcel.exportExcel(sheetName, titleName, headers, dataSet, resultUrl, pattern);
         return "success";
     }
+
     //poi导入
     @ResponseBody
     @RequestMapping("importExcel")
-    public String importExcel() throws Exception{
-        FileSystemView fsv = FileSystemView.getFileSystemView();
+    public String importExcel(MultipartFile file) throws Exception {
+      /*  FileSystemView fsv = FileSystemView.getFileSystemView();
         File com=fsv.getHomeDirectory();
-        String originUrl=com+"\\"+format+".xls";
-        int startRow=2;
-        int endRow=0;
-        List<TitleInfo> titleList = (List<TitleInfo>) ImportExcel.importExcel(originUrl, startRow, endRow, TitleInfo.class);
-        for (TitleInfo title : titleList) {
+        System.out.println(com);*/
+        //String originUrl=com+"\\"+format+".xls";
 
-            titleService.importTitle(title);
+        String filePath = file.getOriginalFilename();
+        System.out.println(filePath);
+        String path = filePath.substring(filePath.lastIndexOf("."));
+        if (path.equals(".xls")) {
+            int startRow = 2;//从表格的哪一行开始读取
+            int endRow = 0;
+            List<TitleInfo> titleList = (List<TitleInfo>) ImportExcel.importExcel(file.getInputStream(), startRow, endRow, TitleInfo.class);
+            for (TitleInfo title : titleList) {
+                titleService.importTitle(title);
+            }
+            return "1";//成功
+        } else {
+            return "0";
         }
-        return "";
     }
-
 
 
 }
